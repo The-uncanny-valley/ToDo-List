17c1be236f6955fc89dea2539e6874465db6ccbb
the-uncanny-valley <runcannyvalley@gmail.com>
2025-05-08 18:30:22 +0300

    add ArrayList for notes and implement showNotes() method to display them

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,21 +1,29 @@
 package com.example.todolist;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     private LinearLayout linearLayoutNotes;
     private FloatingActionButton buttonAddNote;
-    
+
+    private ArrayList<Note> notes = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +36,50 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         initViews();
+
+        Random random = new Random();
+        for (int i = 0; i < 20; i++) {
+            Note note = new Note(i, "Note " + i, random.nextInt(3));
+            notes.add(note);
+        }
+
+        showNotes();
     }
 
     private void initViews() {
         linearLayoutNotes = findViewById(R.id.linearLayoutNotes);
         buttonAddNote = findViewById(R.id.buttonAddNote);
+    }
+
+    private void showNotes() {
+        for (Note note : notes) {
+            View view = getLayoutInflater().inflate(
+                    R.layout.note_item,
+                    linearLayoutNotes,
+                    false);
+
+            TextView textViewNote = view.findViewById(R.id.textViewNote);
+            textViewNote.setText(note.getText());
+
+            int colorResId;
+
+            switch (note.getPriority()) {
+                case 0:
+                    colorResId = R.color.surface_default;
+                    break;
+                case 1:
+                    colorResId = R.color.primary_main;
+                    break;
+                default:
+                    colorResId = R.color.secondary_main;
+            }
+
+            int color = ContextCompat.getColor(this, colorResId);
+            textViewNote.setBackgroundColor(color);
+
+            linearLayoutNotes.addView(view);
+
+
+        }
     }
 }
